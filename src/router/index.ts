c2c7be41c2ed5f1714fefee569b7776f08fa40b8
crimsonvan <2329016850@qq.com>
2024-11-17 23:57:10 +0800

    测试首次提交

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  //路由模式hash
+  history: createWebHashHistory('/tstemplate/'),
+  routes: [
+    {
+      //登录成功以后展示数据的路由
+      path: '/',
+      component: () => import('../views/layout/index.vue'),
+      name: 'layout'
+    },
+    {
+      path: '/login',
+      component: () => import('../views/login/index.vue'),
+      name: 'login'
+    }
+  ]
+})
+
+export default router
